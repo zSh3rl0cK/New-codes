611c3dfff9981304ba0524f117455e58646a4ba0
Pedro Henrique Ribeiro Dias <125469206+zSh3rl0cK@users.noreply.github.com>
2024-11-19 16:06:14 -0300

    Update Contador - projeto E08 L3.vhd

--- Contador - projeto E08 L3.vhd
+++ Contador - projeto E08 L3.vhd
@@ -3,14 +3,14 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity PJ_E08_L3_PEDRO_DIAS_529_FELIPE_TAGAWA_2037_FELIPE_LOSCHI_601 is
     Port( clock : in  STD_LOGIC;
-           Reset : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
            output : buffer integer range 0 to 7);
 end PJ_E08_L3_PEDRO_DIAS_529_FELIPE_TAGAWA_2037_FELIPE_LOSCHI_601;
 
 architecture Behavioral of PJ_E08_L3_PEDRO_DIAS_529_FELIPE_TAGAWA_2037_FELIPE_LOSCHI_601 is
 
 begin
-process(Clock, Reset) 
+process(clock, reset) 
 begin
    if(Reset = '1') then 
 	output <= 0;
