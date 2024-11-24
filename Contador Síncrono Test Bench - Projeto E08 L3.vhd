75d7b239e1de02ff6b98f9817e04557c40fa1b5b
Pedro Henrique Ribeiro Dias <125469206+zSh3rl0cK@users.noreply.github.com>
2024-11-24 20:04:35 -0300

    Update Contador Síncrono Test Bench - Projeto E08 L3.vhd

--- Contador Síncrono Test Bench - Projeto E08 L3.vhd
+++ Contador Síncrono Test Bench - Projeto E08 L3.vhd
@@ -5,102 +5,48 @@ ENTITY nome IS
 END nome;
  
 ARCHITECTURE behavior OF nome IS 
- 
+
     -- Component Declaration for the Unit Under Test (UUT)
- 
-    COMPONENT nome2
+    COMPONENT PJ_E08_L3_PEDRO_DIAS_529_FELIPE_TAGAWA_2037_FELIPE_LOSCHI_601
     PORT(
          clock : IN  std_logic;
          reset : IN  std_logic;
          output : buffer integer range 0 to 7
         );
     END COMPONENT;
-    
 
-   --Inputs
-   signal clock : std_logic := '0';
-   signal reset : std_logic := '0';
-   signal output : buffer integer range 0 to 7
-
- 	--Outputs
-   signal output : integer range 0 to 7;
-BEGIN
+    --Inputs
+    signal clock : std_logic := '0';
+    signal reset : std_logic := '0';
+    signal output : integer range 0 to 7;
  
-	-- Instantiate the Unit Under Test (UUT)
-   uut: nome PORT MAP (
-          clock => clock,
-          reset => reset,
-          output => output
-        );
-     
-   -- Stimulus process
-   stim_proc: process
-   begin		
-      
-		reset <= '1';
-      wait for 100 ns;
-      reset <= '0';
-      wait for 100 ns;	
+BEGIN
 
-		clock <= '0'; 
-      wait for 100 ns;	
-		clock <= '1'; 
-      wait for 100 ns;
-		clock <= '0'; 
-      wait for 100 ns;
-		
-		clock <= '0'; 
-      wait for 100 ns;	
-		clock <= '1'; 
-      wait for 100 ns;
-		clock <= '0'; 
-      wait for 100 ns;
-		
-		clock <= '0'; 
-      wait for 100 ns;	
-		clock <= '1'; 
-      wait for 100 ns;
-		clock <= '0'; 
-      wait for 100 ns;
-		
-		clock <= '0'; 
-      wait for 100 ns;	
-		clock <= '1'; 
-      wait for 100 ns;
-		clock <= '0'; 
-      wait for 100 ns;
-		
-		clock <= '0'; 
-      wait for 100 ns;	
-		clock <= '1'; 
-      wait for 100 ns;
-		clock <= '0'; 
-      wait for 100 ns;
-		
-		
-		clock <= '0'; 
-      wait for 100 ns;	
-		clock <= '1'; 
-      wait for 100 ns;
-		clock <= '0'; 
-      wait for 100 ns;
-		
-		
-		clock <= '0'; 
-      wait for 100 ns;	
-		clock <= '1'; 
-      wait for 100 ns;
-		clock <= '0'; 
-      wait for 100 ns;
-		
-		clock <= '0'; 
-      wait for 100 ns;	
-		clock <= '1'; 
-      wait for 100 ns;
-		clock <= '0'; 
-      wait for 100 ns;
-		
-      wait;
-   end process;
+    -- Instantiate the Unit Under Test (UUT)
+    uut: PJ_E08_L3_PEDRO_DIAS_529_FELIPE_TAGAWA_2037_FELIPE_LOSCHI_601
+    PORT MAP (
+        clock => clock,
+        reset => reset,
+        output => output
+    );
+
+    -- Clock process
+    clock_process : process
+    begin
+        while true loop
+            clock <= not clock;
+            wait for 10 ns;
+        end loop;
+    end process;
+
+    -- Reset process
+    stim_proc: process
+    begin       
+        -- Apply reset
+        reset <= '1';
+        wait for 20 ns;
+        reset <= '0';
+        wait;
+    end process;
 
 END;
